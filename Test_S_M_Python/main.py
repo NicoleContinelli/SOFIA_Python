53c6da4943661bf2d19d92428e04976619b2f0e2
humasoft <nicolecofl@unisabana.edu.co>
2021-11-11 09:37:00 +0100

    Updating the code

--- Test_S_M_Python/main.py
+++ Test_S_M_Python/main.py
@@ -26,10 +26,10 @@ cols = ['Inclination', 'Orientation', 'M1', 'M2', 'M3']
 data = []
 
 # Inclination's repetition
-for inclination in range(5, 51, 5):
+for inclination in range(25, 51, 5):
 # Orientation's repetition
     for orientation in range(5, 360, 30):
-        kine1 = InverseKinematics(tilt, orientation)  # instantiate InverseKinematics class
+        kine1 = InverseKinematics(inclination, orientation)  # instantiate InverseKinematics class
         theta1, theta2, theta3 = kine1.neckInverseKinematics()  # saving the length's cables
 
         motors.setupPositionsMode(10, 10)
@@ -64,4 +64,4 @@ for inclination in range(5, 51, 5):
 
 df = pd.DataFrame(data, columns = cols)  # adding the data values (array type), to the data frame
 print(df)
-df.to_csv(r'/home/humasoft/SOFIA_Python/Data/sensor_motor_7.csv', index = False)
+df.to_csv(r'/home/humasoft/SOFIA_Python/Data/sensor_motor_prueba2.csv', index = False)
