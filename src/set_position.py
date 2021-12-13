3f6faad98585867412140f7333ea83250506ec68
humasoft <nicolecofl@unisabana.edu.co>
2021-12-13 10:15:54 +0100

    Adding more data

--- src/set_position.py
+++ src/set_position.py
@@ -12,7 +12,7 @@ motors = SystemMotors(3)  # instantiate SystemMotors class >> number of motors
 motors.loadMotors([1, 2, 3])  # motor's ids
 motors.startMotors()  # start motors
 
-motors.setupPositionsMode(15, 15) #setting velocity and acceleration values
+motors.setupPositionsMode(12, 12) #setting velocity and acceleration values
 motors.setPositions([0, 0, 0]) 
 
 '''
