becb80958a1f54cfe681051acdb558bf4c334913
humasoft <nicolecofl@unisabana.edu.co>
2022-02-17 12:53:39 +0100

    Fixing some code in main_realtime_training.py

--- src/main_realtime_training.py
+++ src/main_realtime_training.py
@@ -1,10 +1,10 @@
 import math
 import numpy as np
 import pandas as pd
-
+import joblib  
+import time
 
 from model.system_motors import SystemMotors
-from model.inverse_kinematics import InverseKinematics
 from model.sensor import Sensor
 from model.data_collection import DataCollection
 
@@ -27,39 +27,61 @@ motors.setupPositionsMode(12, 12) # setting velocity and acceleration values
 #Load the data with the predicted values
 data_pred = pd.read_csv('/home/humasoft/SOFIA_Python/ml/predicted_data_ANN_3files_optparams.csv')
 
+#ML
+model_reg = joblib.load('/home/humasoft/trained_model_reg_optiparam.pkl')
 
-# Inclination's repetition
-for inclination in range(5, 41, 10):
-   
-# Orientation's repetition
-    for orientation in range(5, 360, 45):
+#Target Values
+inclination = 24.6
+orientation = 107.9
+error_i = 5
+error_o = 5
 
-        row = data_pred[data_pred['I'] == inclination] [data_pred['O'] == orientation] #row with the predicted encoders
-        
-        # Set theta values >> looking for them in the "row" variable
-        theta1 = row.iloc[0,2] 
-        theta2 = row.iloc[0,3]
-        theta3 = row.iloc[0,4]
+#row with the predicted encoders
+row = data_pred[data_pred['I'] == inclination] [data_pred['O'] == orientation]
+print(row)
 
-        motors.setPositions([theta1, theta2, theta3])
+'''
+# Set theta values >> looking for them in the "row" variable
+theta1 = row.iloc[0,2] 
+theta2 = row.iloc[0,3]
+theta3 = row.iloc[0,4]
 
-        # Knowing the Inclination and Orientation of the sensor, with a previous motor position
-        for i in np.arange(0, 2, 0.02):  # Sampling time with steps of 0.02
-            sensor_data = data_col.data_neck_sensor(mi_sensor) # calling data_neck_sensor function (passing fromr adians to degrees)
-            sensor_data_final = list(sensor_data) # converting a tuple to list
-            
-            motor_data = data_col.data_neck_motors(motors) # calling data_neck_motors function (collecting the encoders values)
-            motor_data_final = list(motor_data) # converting a tuple to list
-            
-            data.append([sensor_data_final[0], 
-                         sensor_data_final[1], 
-                         motor_data_final[0], 
-                         motor_data_final[1], 
-                         motor_data_final[2]])
+motors.setPositions([theta1, theta2, theta3])
+time.sleep(3)
+'''
 
-    df = pd.DataFrame(data, columns = cols)  # adding the data values (array type), to the data frame
-    df.to_csv('/home/humasoft/SOFIA_Python/data/data_february/predicted_data_ANN_3files_optparams.csv', index = False)
-    #df.info() or print(df)
+while(np.abs(error_i) > 0.8 or np.abs(error_o) > 7.2):
+    #for i in np.arange(0, 30, 1):  # Sampling time with steps of 0.02
+        sensor_data = data_col.data_neck_sensor(mi_sensor) # calling data_neck_sensor function (passing fromr radians to degrees)
+        sensor_data_final = list(sensor_data) # converting a tuple to list
+        print(sensor_data_final)
+        error_i = (inclination - sensor_data_final[0])
+        sensor_data_final[0] += error_i
+
+        error_o = (orientation - sensor_data_final[1])
+        sensor_data_final[1] += error_o
+
+        #print(error_i,error_o)
+
+        pred = model_reg.predict([[sensor_data_final[0],sensor_data_final[1]]])
+        thetas = pred.flatten().tolist()
+        #print(sensor_data_final[0], sensor_data_final[1], thetas)
+
+        motors.setPositions([thetas[0], thetas[1], thetas[2]])
+
+        motor_data = data_col.data_neck_motors(motors) # calling data_neck_motors function (collecting the encoders values)
+        motor_data_final = list(motor_data) # converting a tuple to list
+
+        data.append([sensor_data_final[0], 
+                        sensor_data_final[1], 
+                        motor_data_final[0], 
+                        motor_data_final[1], 
+                        motor_data_final[2]])
+
+        df = pd.DataFrame(data, columns = cols)  # adding the data values (array type), to the data frame
+        df.to_csv('/home/humasoft/SOFIA_Python/data/data_february/data_ANN_3files_optparams_control.csv', index = False)
+        #df.info() or print(df)
+        #print(df)
     
 
 print("Data Ready")
